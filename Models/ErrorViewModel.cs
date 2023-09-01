f0f42f4ab0c2bf012476bd232b3b2881aff47b6a
seandeo <adeoyeseun@hotmail.com>
2023-08-31 20:56:30 -0500

    1st commit change on Aug 31st

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Aug31_Version1_WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
